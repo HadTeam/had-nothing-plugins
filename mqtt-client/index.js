56db5ca924d88b7a4c9528a2023a430c57fa2364
CornWorld <cw@had.ink>
2022-05-04 20:41:51 +0800

    [Clean] Reformat code

--- mqtt-client/index.js
+++ mqtt-client/index.js
@@ -1,44 +1,42 @@
 import * as mqtt from "mqtt";
 import {waitFor} from "wait-for-event";
 
-let pathHandler=new Map();
+let pathHandler = new Map();
 
 export default async function Factory() {
     let client;
     try {
-        client=mqtt.connect(global.config["mqtt"]["brokerUrl"]);
-    }
-    catch (err) {
-        console.error("[MqttClient]Connection error, err: ",err);
+        client = mqtt.connect(global.config["mqtt"]["brokerUrl"]);
+    } catch (err) {
+        console.error("[MqttClient]Connection error, err: ", err);
     }
     client.on('connect', () => {
         console.log("[MqttClient]Connection built, listening...");
     });
-    client.on('message',(topic, message)=>{
-        let messageJson=JSON.parse(message.toString());
+    client.on('message', (topic, message) => {
+        let messageJson = JSON.parse(message.toString());
         // console.debug("[MqttClient]Message arrived, topic:",topic,",message:",messageJson);
-        if(pathHandler.has(topic)) {
-            let handlers=pathHandler.get(topic);
-            handlers.forEach((handler)=>{
+        if (pathHandler.has(topic)) {
+            let handlers = pathHandler.get(topic);
+            handlers.forEach((handler) => {
                 handler(messageJson);
             })
-        }
-        else if(messageJson["jsonrpc"]===2.0) {
-            let eventName="rpc/"+topic;
+        } else if (messageJson["jsonrpc"] === 2.0) {
+            let eventName = "rpc/" + topic;
             global.events.emit(eventName, messageJson);
         }
     });
-    client.addHandler=(topic, cb)=>{
-        if(!pathHandler.has(topic)) {
+    client.addHandler = (topic, cb) => {
+        if (!pathHandler.has(topic)) {
             pathHandler.set(topic, []);
             client.subscribe(topic);
         }
         pathHandler.get(topic).push(cb);
     }
-    client.removeHandler=(topic, cb)=>{
-        if(!pathHandler.has(topic)) return false;
-        let cbs=pathHandler.get(topic);
-        if(cbs.includes(cb)) {
+    client.removeHandler = (topic, cb) => {
+        if (!pathHandler.has(topic)) return false;
+        let cbs = pathHandler.get(topic);
+        if (cbs.includes(cb)) {
             cbs.remove(cb);
         }
     }
--- mqtt-component/index.js
+++ mqtt-component/index.js
@@ -5,15 +5,16 @@ import pTimeout from "p-timeout";
 export default function Factory() {
     return new MqttComponent();
 }
+
 class MqttComponent {
     constructor() {
         global.plugins.mqttClient.addHandler("panel/regId", this.regId);
     }
     
     async regId(arg) {
-        let uuid=arg["params"][0];
+        let uuid = arg["params"][0];
         
-        const genJsonrpc2Req=(action, params, id)=>{
+        const genJsonrpc2Req = (action, params, id) => {
             return JSON.stringify({
                 "jsonrpc": 2.0,
                 "method": action,
@@ -22,23 +23,25 @@ class MqttComponent {
             });
         }
         
-        const componentHandler=async (uuid, action, param)=>{
-            if(!global.plugins.mqttClient.connected) return undefined;
+        const componentHandler = async (uuid, action, param) => {
+            if (!global.plugins.mqttClient.connected) return undefined;
             
-            let reqId=crypto.randomBytes(8).toString('hex');
-            let req=genJsonrpc2Req(action, param, reqId);
-            let eventName="rpc/panel/component/response/"+uuid+"/"+reqId;
-            let ret={};
+            let reqId = crypto.randomBytes(8).toString('hex');
+            let req = genJsonrpc2Req(action, param, reqId);
+            let eventName = "rpc/panel/component/response/" + uuid + "/" + reqId;
+            let ret = {};
             
-            global.plugins.mqttClient.subscribe("panel/component/response/"+uuid+"/"+reqId);
-            global.events.once(eventName, (res)=>{ ret=res.result; });
-            global.plugins.mqttClient.publish("panel/component/"+uuid, req);
+            global.plugins.mqttClient.subscribe("panel/component/response/" + uuid + "/" + reqId);
+            global.events.once(eventName, (res) => {
+                ret = res.result;
+            });
+            global.plugins.mqttClient.publish("panel/component/" + uuid, req);
             try {
                 await pTimeout(waitFor(eventName, global.events), 1000);
             } catch (err) {
-                console.warn("[MqttComponent]Request"+JSON.stringify({id:reqId, action: action})+" timeout.");
+                console.warn("[MqttComponent]Request" + JSON.stringify({id: reqId, action: action}) + " timeout.");
             }
-            global.plugins.mqttClient.unsubscribe("panel/component/response/"+uuid+"/"+reqId);
+            global.plugins.mqttClient.unsubscribe("panel/component/response/" + uuid + "/" + reqId);
             
             return ret;
         }
